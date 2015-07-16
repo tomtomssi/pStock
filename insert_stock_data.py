206ad519776f4b6b0d4743fac5a1f02ba963d782
tomtomssi <tatu.solla@gmail.com>
2015-07-16 19:33:29 +0300

    Update single stocks in threads
    Add a simple queue and threads for updating single stocks

--- insert_stock_data.py
+++ insert_stock_data.py
@@ -1,17 +1,22 @@
 __author__ = 'tomtomssi'
 import urllib.request
+import time
+import threading
+from queue import Queue
 
 import psycopg2
 from bs4 import BeautifulSoup as Soup
 
 import config_parser
 
+lock = threading.Lock()
+q = Queue()
+
 
 def query_for_abbreviations():
     config = config_parser.ParseConfig()
 
     conn_string = "host='{0}'dbname='{1}'user='{2}'password='{3}'".format(config.get_host(), config.get_database(), config.get_username(), config.get_password())
-    print("Connecting to database\n")
 
     connection = psycopg2.connect(conn_string)
 
@@ -19,7 +24,11 @@ def query_for_abbreviations():
 
     cursor.execute("SELECT * from abbreviations ORDER BY name")
 
-    query_for_data(connection, cursor)
+    data = cursor.fetchall()
+
+    connection.close()
+
+    return data
 
 
 def get_stock_data(abbreviation):
@@ -31,14 +40,44 @@ def get_stock_data(abbreviation):
     return name[1].contents[0]
 
 
-def query_for_data(connection, cursor):
-    for item in cursor.fetchall():
-        print("INSERT INTO data(name, abbreviation_key) "
-                       "VALUES (%s, %s)", [get_stock_data(item[0]), item[1]])
+def query_for_data(item):
+    config = config_parser.ParseConfig()
 
-        cursor.execute("INSERT INTO data(name, abbreviation_key) "
-                       "VALUES (%s, %s)", [get_stock_data(item[0]), item[1]])
-        connection.commit()
+    conn_string = "host='{0}'dbname='{1}'user='{2}'password='{3}'".format(config.get_host(), config.get_database(), config.get_username(), config.get_password())
 
-query_for_abbreviations()
+    connection = psycopg2.connect(conn_string)
+
+    cursor = connection.cursor()
+
+    print("INSERT INTO data(name, abbreviation_key) "
+                   "VALUES (%s, %s)", [get_stock_data(item[0]), item[1]])
+
+    cursor.execute("INSERT INTO data(name, abbreviation_key) "
+                   "VALUES (%s, %s)", [get_stock_data(item[0]), item[1]])
+
+    connection.commit()
+
+    connection.close()
+
+
+def worker():
+    while True:
+        item = q.get()
+        query_for_data(item)
+        q.task_done()
+
+
+for i in range(5):
+    t = threading.Thread(target=worker)
+    t.daemon = True
+    t.start()
+
+start = time.perf_counter()
+
+for abbreviation in query_for_abbreviations():
+    q.put(abbreviation)
+
+q.join()
+
+print('time:', time.perf_counter() - start)
 
